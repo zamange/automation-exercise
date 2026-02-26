a045584385b5cc25bdd636a9f0beee9a2f00f770
Zamagcina Ngeyane <zamagcinangeyane6@gmail.com>
2026-02-26 14:31:48 +0200

    updated SignUpPage methods

--- UItests/pages/SignUpPage.cs
+++ UItests/pages/SignUpPage.cs
@@ -7,7 +7,7 @@ namespace pages
     public class SignUpPage
     {
         private IWebDriver driver;
-        private By newUserText = By.XPath("//h2[text()='New User Signup!']");
+        private By signUpPage = By.XPath("//a[contains(text(),'Signup')]");        private By newUserText = By.XPath("//h2[text()='New User Signup!']");
 
         private By nameInput = By.Name("name");
 
@@ -27,30 +27,44 @@ namespace pages
         private By userDayOfBirth = By.Id("days");
         private By userMonthOfBirth = By.Id("months");
         private By userYearOfBirth = By.Id("years");
-        private By newsLetterCheck = By.Id("");
-        private By offersCheck = By.Id("");
-        private By firstName = By.Id("");
-        private By lastName = By.Id("");
-        private By company = By.Id("");
-        private By address = By.Id("");
-        private By address2 = By.Id("");
-        private By country = By.Id("");
-        private By state = By.Id("");
-        private By city = By.Id("");
-        private By zipCode = By.Id("");
-        
-        
+        private By newsLetterCheck = By.Id("newsletter");
+        private By offersCheck = By.Id("optin");
+        private By firstName = By.Id("first_name");
+        private By lastName = By.Id("last_name");
+        private By company = By.Id("company");
+        private By address = By.Id("address1");
+        private By address2 = By.Id("address2");
+        private By country = By.Id("country");
+        private By state = By.Id("state");
+        private By city = By.Id("city");
+        private By zipCode = By.Id("zipcode");
+
+        private By mobileNumber = By.Id("mobile_number");
+        private By createAccount = By.Id("button");
+        private By accountCreatedText = By.Id("form");
+        private By continueButton = By.XPath("/html/body/section/div/div[1]/div/div/a");
+        private By loggedInAs = By.Id("header");
+        private By deleteAccount = By.XPath("/html/body/header/div/div/div/div[2]/div/ul/li[5]/a/text()");
+        private By accountDeletedText = By.Id("form");
+        private By continueDeleteButton = By.CssSelector("a[data-qa='continue-button']");        
         public SignUpPage(IWebDriver driver)
         {
             this.driver = driver;
         }
+
+        public void ClickSignUp()
+        {
+            driver.FindElement(signUpButton).Click();
+        }
+
+        public bool VerifySignUpPage()
+        {
+            return driver.FindElement(homePageIcon).Displayed;
+        }
         
         
 
-        // public void ClickSignUp()
-        // {
-        //     driver.FindElement(signupButton).Click();
-        // }
+
 
         
     }
